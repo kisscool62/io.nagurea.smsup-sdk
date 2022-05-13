532587059bf4f241fb61512e87cfce522ad60591
Pascal AUREGAN <kisscool62@gmail.com>
2022-05-13 21:49:02 +0200

    added E164 format tests

--- src/test/java/io/nagurea/smsupsdk/common/e164/E164HelperTest.java
+++ src/test/java/io/nagurea/smsupsdk/common/e164/E164HelperTest.java
@@ -1,21 +1,45 @@
 package io.nagurea.smsupsdk.common.e164;
 
-import org.junit.jupiter.api.Test;
+import io.nagurea.smsupsdk.common.exception.E164FormatException;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class E164HelperTest {
 
-    @Test
-    void checkIsOk() {
+    @ParameterizedTest(name = "{index} => phoneNumberToTest={0}, shouldRaiseException={1}")
+    @MethodSource
+    void checkPhoneNumbersComplyE164Rules(final String phoneNumberToTest, boolean shouldRaiseException) {
         // given
-        final String phoneNumberToTest = "41781234567";
+        // According to method parameters
 
         // when
-        E164Helper.check(phoneNumberToTest);
-        final boolean hasSucceeded = true;
+        boolean hasRaisedException = false;
+        try {
+            E164Helper.check(phoneNumberToTest);
 
+        }catch (E164FormatException e){
+            hasRaisedException = true;
+        }
         // then
-        assertTrue(hasSucceeded);
+        assertEquals(shouldRaiseException, hasRaisedException);
+
     }
+
+    private static Stream<Arguments> checkPhoneNumbersComplyE164Rules(){
+        return Stream.of(
+                arguments("41781234567", false),
+                arguments("+41781234567", false),
+                arguments("0781234567", true),
+                arguments("41 78 123 45 67", true),
+                arguments("33781234567", false)
+        );
+    }
+
+
 }
